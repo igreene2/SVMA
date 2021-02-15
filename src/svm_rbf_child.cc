727318c0c8861e2d7a04bc25de044deae1f4ec8f
Isabel Greene <igreene@dynamic-oit-vapornet100-10-9-43-61.princeton.edu>
2021-02-15 14:42:34 -0500

    cmake

--- src/svm_rbf_child.cc
+++ src/svm_rbf_child.cc
@@ -73,7 +73,7 @@ void DefineRBFChild(Ila& m) {
         // look into == vs >
         // probs gonna have to do a signed comparision uninterpreted function, but maybe == is sufficient
         instr.SetUpdate(m.state("child_state"), Ite(byte_cnt == m.state("fv_dim"), 
-        BvConst(2, 2), BvConst(1, 2));
+        BvConst(2, 2), BvConst(1, 2)));
 
     }
 
