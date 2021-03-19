da317c40606999e4105c9f3cd0998da995b49ccc
Isabel Greene <igreene@dynamic-oit-vapornet100-10-9-43-61.princeton.edu>
2021-03-19 17:14:00 -0400

    testbench

--- bench/svma_testbench.cc
+++ bench/svma_testbench.cc
@@ -33,7 +33,7 @@ SC_MODULE(Source) {
     input_done = 0;
     // read program fragment from file
     std::ifstream fin;
-    fin.open("./sim_info/test_input_svma_linprec.json", ios::in);
+    fin.open("./sim_info/test_input_svma_rbf.json", ios::in);
     
     //parse the json file
     json cmd_seq;
