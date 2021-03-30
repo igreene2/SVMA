25041d1814cd1ea19394125c3c408d42254b5452
Isabel Greene <igreene@dynamic-oit-vapornet100-10-9-43-61.princeton.edu>
2021-03-30 01:30:59 -0400

    testbench

--- uninterpreted_func/uninterpreted_func.cc
+++ uninterpreted_func/uninterpreted_func.cc
@@ -1,8 +1,8 @@
-#include <SVMA.h>
-#include <systemc.h>
-#include <math.h>
+include <SVMA.h>
+include <systemc.h>
+include <math.h>
 
-#include <fstream>
+include <fstream>
 
 
 sc_biguint<32> SVMA::Mult(sc_biguint<32> mult1, sc_biguint<32> mult2) {
@@ -10,20 +10,10 @@ sc_biguint<32> SVMA::Mult(sc_biguint<32> mult1, sc_biguint<32> mult2) {
   sc_bigint<32> arg1 = mult1.to_int();
   sc_bigint<32> arg2 = mult2.to_int();
 
-  float f1 = (*(float*)&arg1);
-  float f2 = (*(float*)&arg2);
-
-  float rezzy = f1 * f2;
-  //sc_bigint<32> ires = arg1 * arg2;
-
-  int ires = *(int*)&rezzy;
-  sc_bigint<32> res = ires.to_int();
-
-
-
+  sc_bigint<32> res = arg1 * arg2;
   
-  sc_biguint<32> result = res;
-  return result;
+  sc_bigint<32> result_s = res;
+  sc_biguint<32> result = result_s;
 }
 
 sc_biguint<32> SVMA::Sub(sc_biguint<32> sub1, sc_biguint<32> sub2) {
@@ -31,16 +21,8 @@ sc_biguint<32> SVMA::Sub(sc_biguint<32> sub1, sc_biguint<32> sub2) {
   sc_bigint<32> sub1_s = sub1;
   sc_bigint<32> sub2_s = sub2;
 
-  float f1 = (*(float*)&sub1_s);
-  float f2 = (*(float*)&sub2_s);
-
-  float rezzy = f1 - f2;
-
-  int ires = *(int*)&rezzy;
-  sc_bigint<32> res = ires.to_int();
-
-  //sc_bigint<32> out = sub1_s - sub2_s;
-  sc_bigint<32> result_s = res.to_int();
+  sc_bigint<32> out = sub1_s - sub2_s;
+  sc_bigint<32> result_s = out.to_int();
   sc_biguint<32> result = result_s;
 
   return result;
@@ -50,7 +32,6 @@ sc_biguint<32> SVMA::Sub(sc_biguint<32> sub1, sc_biguint<32> sub2) {
 sc_biguint<32> SVMA::Exponent(sc_biguint<32> exponent) {
 
   sc_bigint<32> arg1 = exponent.to_int();
-  
   double arg0 = arg1.to_double();
 
   // need exponent to be double
@@ -63,11 +44,11 @@ sc_biguint<32> SVMA::Exponent(sc_biguint<32> exponent) {
 }
 
 sc_biguint<32> SVMA::Shift(sc_biguint<32> base, sc_biguint<32> shiftamt) {
-  
+
   sc_bigint<32> base_s = base;
   sc_bigint<32> shiftamt_s = shiftamt;  
 
-  sc_bigint<32>  base_shift = base_s >> shiftamt_s;
+  sc_bigint<32> base_shift = base_s >> shiftamt_s;
   sc_biguint<32> result = base_shift;
 
   return result;
